7eebeabed0fe33f363627a851fe0bb3a56d9ac55
Connor J Thompson <105184104+Connor-jt@users.noreply.github.com>
2023-07-24 12:41:59 +1000

    Update code_utils.cs

--- code_utils.cs
+++ code_utils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -15,11 +15,9 @@ namespace Infinite_module_test
         // DEBUG METHOD // DEBUG METHOD // DEBUG METHOD //
         public static unsafe T KindaSafe_SuperCast<T>(byte[] data) // double checks that the address is correct by comparing the actual size of the array with the size at he address, should not be needed.
         {
-            // apparently arrays are structured differently between .net versions, great lol
-            ulong data_ptr = *(ulong*)&data;
-            if (*(ulong*)(data_ptr + 0x8) == (ulong)data.Length) // 0x08 is the byte count
-                return *(T*)(data_ptr + 0x10); // 0x10 is the start of the actual data
-            return default(T);
+            fixed (byte* first = &data[0]) {
+                return *((T*)first);
+            }
         }
         // DEBUG METHOD // DEBUG METHOD // DEBUG METHOD //
         public static unsafe T KindaSafe_SuperCast<T>(byte[] data, ulong startIndex)
